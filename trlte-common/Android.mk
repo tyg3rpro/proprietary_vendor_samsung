7c52ac0d13e50c716231b443a1402111ec0d79c8
underscoremone <underscoremone@gmail.com>
2017-09-20 01:50:02 +0100

    trlte*: Regen and update variants blobs
    * trltespr: N910PVPS4DQG1
    * trltetmo: N910TUVS2EQG1
    * trltexx:  N910FXXS1DQH9
    
    Change-Id: I3d612029187da99b4440a7594e02aff821d4cc36

--- trlte-common/Android.mk
+++ trlte-common/Android.mk
@@ -1,10 +1,11 @@
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2014-2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,7 +17,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter trlte trltecan trltexx trltetmo trltespr trlteusc trltevzw,$(TARGET_DEVICE)),)
+ifneq ($(filter trlte trltecan trltespr trltetmo trlteusc trltevzw trltexx,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libmm-abl
@@ -39,4 +40,3 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_PREBUILT)
 
 endif
-
